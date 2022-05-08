3ed2695d9d571bc1d9dee09f06fad03cd157aad8
alexander11902 <wirebuilder28@users.noreply.github.com>
2022-05-08 18:01:25 +0200

    +

--- typescript_aufgabe/hallo_welt.js
+++
@@ -1,6 +0,0 @@
-console.log("Hallo welt!");
-const deImport2021 = 1203;
-const deExport2021 = 1375;
-const BRD = "Bundesrepublik Deutschland";
-console.log("2021 hat die " + BRD + " waren im wert von " + deImport2021 + " Milliarden Euro Importiert");
-//# sourceMappingURL=hallo_welt.js.map
