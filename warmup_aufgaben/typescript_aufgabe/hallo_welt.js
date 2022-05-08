0d16c20d4df2eec5d45e57ebabddd3d340e73471
alexander11902 <wirebuilder28@users.noreply.github.com>
2022-05-08 15:24:47 +0200

    added typescript

---
+++ warmup_aufgaben/typescript_aufgabe/hallo_welt.js
@@ -0,0 +1,5 @@
+console.log("Hallo welt!");
+var deImport2021 = 1203;
+var deExport2021 = 1375;
+var BRD = "Bundesrepublik Deutschland";
+console.log("2021 hat die " + BRD + " waren im wert von " + deImport2021 + " Milliarden Euro Importiert");
--- warmup_aufgaben/typescript_aufgabe/hallo_welt.ts
+++ warmup_aufgaben/typescript_aufgabe/hallo_welt.ts
@@ -1,1 +1,7 @@
-alert("Hallo welt!");
+console.log("Hallo welt!");
+
+const deImport2021:number = 1203;
+const deExport2021:number = 1375;
+const BRD:string = "Bundesrepublik Deutschland";
+
+console.log("2021 hat die "+BRD+" waren im wert von "+deImport2021+" Milliarden Euro Importiert")
