71ac529fe0031124990af83e1b69e161c4fc9f64
alexander11902 <wirebuilder28@users.noreply.github.com>
2022-06-22 18:52:53 +0200

    +

--- Aufgabe_9/drumpad.js
+++ Aufgabe_9/drumpad.js
@@ -20,18 +20,32 @@ function clicked() {
     document.querySelector(".pad_g").addEventListener("click", function () { playSample(soundpad.laugh_1); });
     document.querySelector(".pad_h").addEventListener("click", function () { playSample(soundpad.laugh_2); });
     document.querySelector(".pad_i").addEventListener("click", function () { playSample(soundpad.snare); });
-    document.querySelector(".playbutton").addEventListener("click", soundplay);
+    document.querySelector(".playbutton").addEventListener("click", function () { soundplay(); });
     document.querySelector(".random").addEventListener("click", function () { randombeat(); });
 }
 function playSample(soundpad) {
     var sound = new Audio(soundpad);
     sound.play();
+    sound.volume = 0.5;
+}
+var delay = 0;
+function delaySample(soundpad, delaytime) {
+    var sound = new Audio(soundpad);
+    var delay = 0;
+    var isPlaying = function (sound) {
+        return sound.paused;
+    };
+    //if(isPlaying(sound) == true){ document.querySelector(".random").setAttribute("class","background-image: rgb(255,255,255)");}
+    setInterval(function () {
+        sound.play();
+    }, delay);
+    sound.volume = 0.5;
 }
 function randombeat() {
     for (var index = 0; index < drum_machine.length; index++) {
         var element = drum_machine[index];
-        var rng = Math.floor(Math.random() * drum_machine.length);
-        playSample(drum_machine[rng]);
+        var randint = Math.floor(Math.random() * drum_machine.length);
+        delaySample(drum_machine[randint], 600);
     }
 }
 var drum_machine = [
@@ -40,15 +54,15 @@ var drum_machine = [
     "assets/DrumPad/hihat.mp3"
 ];
 function soundplay() {
-    setInterval(function () {
-        playSample(drum_machine[0]);
-    }, 500);
-    setInterval(function () {
-        playSample(drum_machine[1]);
-    }, 1000);
-    setInterval(function () {
-        playSample(drum_machine[2]);
-    }, 1500);
+    var randint = Math.floor(Math.random() * drum_machine.length);
+    if (document.querySelector(".playbutton").getAttribute("class") == "fas fa-play") {
+        document.querySelector(".playbutton").setAttribute("class", "fas fa-stop");
+        delaySample(drum_machine[randint], 600);
+    }
+    else {
+        document.querySelector(".playbutton").setAttribute("class", "fas fa-play");
+        delaySample(drum_machine[randint], 600);
+    }
 }
 // function random(){
 //     soundpad[Math.floor(Math.random()* soundpad.)];
--- Aufgabe_9/drumpad.ts
+++ Aufgabe_9/drumpad.ts
@@ -25,21 +25,41 @@ function clicked() {
     document.querySelector(".pad_g").addEventListener("click", function() {playSample(soundpad.laugh_1); });
     document.querySelector(".pad_h").addEventListener("click", function() {playSample(soundpad.laugh_2); });
     document.querySelector(".pad_i").addEventListener("click", function() {playSample(soundpad.snare); });
-    document.querySelector(".playbutton").addEventListener("click", soundplay );
-    document.querySelector(".random").addEventListener("click", function() {randombeat();});
+    document.querySelector(".playbutton").addEventListener("click", function(){ soundplay(); });
+    document.querySelector(".random").addEventListener("click", function() { randombeat();});
 }
 
-function playSample(soundpad: string): void {
+function playSample(soundpad: string){
     var sound: HTMLAudioElement = new Audio(soundpad);
     sound.play();
+    sound.volume = 0.5;
+
 }
 
-function randombeat(){
+var delay: number = 0
+
+function delaySample(soundpad: string, delaytime: number){
+    var sound: HTMLAudioElement = new Audio(soundpad);
+    var delay: number = 0;
+    const isPlaying = (sound: { paused: boolean; }) =>{
+        return sound.paused;
+    }
+    //if(isPlaying(sound) == true){ document.querySelector(".random").setAttribute("class","background-image: rgb(255,255,255)");}
+    setInterval(function(){
+        sound.play();   }, delay );
+        sound.volume = 0.5;
+}
+
+
+
+function randombeat(): void{
 
 for (let index = 0; index < drum_machine.length; index++) {
+
     const element = drum_machine[index];
-    const rng = Math.floor(Math.random()* drum_machine.length );
-    playSample(drum_machine[rng]);
+    const randint = Math.floor(Math.random() * drum_machine.length );
+    delaySample( drum_machine[randint], 600 );
+
 }
 
 }
@@ -51,23 +71,19 @@ const drum_machine :string [] = [
      "assets/DrumPad/hihat.mp3"
 ];
 
-function soundplay(): void {
-
-    setInterval(function() {
-        playSample(
-        drum_machine[0]); }, 500);
-    setInterval(function() {
-        playSample(
-        drum_machine[1]
-        );     
-    },          1000);
-    setInterval(function() {
-        playSample(
-        drum_machine[2]);
+function soundplay() {
+    var randint = Math.floor( Math.random() * drum_machine.length );
+    if(document.querySelector(".playbutton").getAttribute("class") == "fas fa-play"){
+        document.querySelector(".playbutton").setAttribute("class", "fas fa-stop");
         
-    },          1500);
-
+            delaySample( drum_machine[randint], 600);
+        }else{
+            document.querySelector(".playbutton").setAttribute("class", "fas fa-play");
+            delaySample(drum_machine[randint],600);
+    }
 }
+
+
 
 
 // function random(){
